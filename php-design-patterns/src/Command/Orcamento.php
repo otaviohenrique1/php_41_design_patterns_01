c9b3ad6ff242fd9025564cc7250c78dc7c698367
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2024-05-20 15:02:34 -0300

    commit 9

---
+++ php-design-patterns/src/Command/Orcamento.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Alura\DesignPattern\Command;
+
+use Alura\DesignPattern\Command\EstadosOrcamento\EmAprovacao;
+use Alura\DesignPattern\Command\EstadosOrcamento\EstadoOrcamento;
+use DomainException;
+
+class Orcamento
+{
+  public int $quantidadeItens;
+  public float $valor;
+  public EstadoOrcamento $estadoAtual;
+
+  public function __construct($estadoAtual) {
+    $this->$estadoAtual = new EmAprovacao();
+  }
+
+  public function aplicaDescontoExtra()
+  {
+    $this->valor -= $this->estadoAtual->calculaDescontoExtra($this);
+  }
+
+  public function aprova()
+  {
+    $this->estadoAtual->aprova($this);
+  }
+
+  public function reprova()
+  {
+    $this->estadoAtual->reprova($this);
+  }
+
+  public function finaliza()
+  {
+    $this->estadoAtual->finaliza($this);
+  }
+}
